e0399c9ffa36f586f4a14b1366afdd53392dbe2d
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-06-05 22:34:38 +0300

    Update authOperations.js

--- redux/auth/authOperations.js
+++ redux/auth/authOperations.js
@@ -6,7 +6,7 @@ import {
     signOut,
 } from 'firebase/auth';
 import { auth } from '../../firebase/config';
-import { authSlice } from './authReducer';
+import { authSlice } from './authSlice';
 
 const signUpOper = ({name, email, password}) => async (dispatch, getState) => {
     try {
